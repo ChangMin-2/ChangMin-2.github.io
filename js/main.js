3b4fbc82ee3fb7dcf0acbc834e77a6da434525cf
changmin <lcmin0302@gamil.com>
2022-08-01 17:55:14 +0900

    second

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// section scroll
+$(function(){
+  $('.nav__container li a').click(function(e){
+    e.preventDefault();
+
+    $(window).scrollTo(this.hash || 0, 1000);
+  })
+
+  $('.contact__a').click(function(e){
+    e.preventDefault();
+
+    $(window).scrollTo(this.hash || 0, 1000);
+  })
+})
+
 // menuBtn click event
 const menuOpen = document.querySelector('.menu_open');
 const menuClose = document.querySelector('.menu_close');
@@ -13,3 +28,38 @@ menuClose.addEventListener('click',function(){
   menuClose.style.display = 'none';
   nav.style.height = '0';
 })
+
+// skills 
+gsap.from(".bar abbr", {
+  scrollTrigger: {
+    trigger: ".bar",
+    // toggleActions: "restart none reverse none",
+  } ,
+  left: 0,
+  ease: Power2.easeInOut,
+  duration: 3,
+  stagger: 0.1,
+})
+gsap.from(".bar span", {
+  scrollTrigger: {
+    trigger: ".bar",
+    // toggleActions: "restart none reverse none",
+  } ,
+  width: 0,
+  ease: Power2.easeInOut,
+  duration: 3,
+  stagger: 0.1,
+})
+
+// projects swiper
+
+const projectsSwiper = new Swiper(".projects__swiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
